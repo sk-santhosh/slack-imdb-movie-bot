35e446b3ed7f770e8896229500f751a33bb19a6b
Santhosh J <santhosh@nextwavemultimedia.com>
2021-02-10 23:52:01 +0530

    heroku

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 require = require("esm")(module /*, options*/);
 module.exports = require("./app.js");
+
+const server = require("http").createServer();
+
+server.listen(3000, () => console.log("Server started in port 3000"));
